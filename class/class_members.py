507e89c5f04e3aeaf8d452e8fdca8d01e677f66d
xuelinli <xuelinli@tencent.com>
2024-09-29 20:44:53 +0800

    fix

--- class/class_members.py
+++ class/class_members.py
@@ -1,48 +1,12 @@
 # coding=utf-8
-"""
-运行结果：
-2024-09-29 20:12:26,457 - root - INFO - set A data to 2
-2024-09-29 20:12:26,457 - root - INFO - A._data=2,id=140518361174720
-2024-09-29 20:12:26,457 - root - INFO - B._data=2,id=140518361174720
-2024-09-29 20:12:26,457 - root - INFO - inst A._data=2,id=140518361174720
-2024-09-29 20:12:26,457 - root - INFO - set A data to 1
-2024-09-29 20:12:26,457 - root - INFO - A._data=1,id=140518361174744
-2024-09-29 20:12:26,457 - root - INFO - B._data=1,id=140518361174744
-2024-09-29 20:12:26,457 - root - INFO - inst A._data=1,id=140518361174744
-2024-09-29 20:12:26,457 - root - INFO - set B data to 3
-2024-09-29 20:12:26,457 - root - INFO - A._data=1,id=140518361174744
-2024-09-29 20:12:26,457 - root - INFO - B._data=3,id=140518361174696
-2024-09-29 20:12:26,457 - root - INFO - inst B._data=3,id=140518361174696
-2024-09-29 20:12:26,457 - root - INFO - set A data to 4
-2024-09-29 20:12:26,458 - root - INFO - A._data=4,id=140518361174672
-2024-09-29 20:12:26,458 - root - INFO - B._data=3,id=140518361174696
-2024-09-29 20:12:26,458 - root - INFO - inst A._data=4,id=140518361174672
-2024-09-29 20:12:26,458 - root - INFO - set B data to 5
-2024-09-29 20:12:26,458 - root - INFO - A._data=4,id=140518361174672
-2024-09-29 20:12:26,458 - root - INFO - B._data=5,id=140518361174648
-2024-09-29 20:12:26,458 - root - INFO - inst B._data=5,id=140518361174648
-
-结论：
-    1. 类成员变量与类绑定，继承时从父类继承对应变量和值
-    2.
-"""
 
 import logging
-import time
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 
 class A:
     _data = 1
-
-    @classmethod
-    def set_a_inner(cls, data):
-        cls._data = data
-
-    @classmethod
-    def set_a_inner2(cls, data):
-        A._data = data
 
     def __init__(self, data=None):
         if data:
